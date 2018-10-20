0a22268e526a22dac60f6c0edc753cb53e90513d
Fernando Orozco Perez <fernando.orozco@globan.com>
2018-10-19 22:32:21 -0500

    Initial commit

---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+// import React from "react";
+import React, { Component } from "react";
+import "./Contact.css";
+import PropTypes from "prop-types";
+
+
+function Contact(props) {
+  return (
+    <div className="contact">
+      <span>{props.name}</span>
+    </div>
+  );
+}
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired
+};
+
+export default Contact;
